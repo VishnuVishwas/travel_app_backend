3be32d11ffe2baae16722c57fbcdc67e2b95d015
VishnuVishwas <gvishnuvishwas60@gmail.com>
2024-09-18 21:18:34 +0530

    added procfile

--- app.py
+++ app.py
@@ -180,5 +180,4 @@ def delete_booking(id):
     return jsonify({'message': 'Booking deleted successfully!'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080)) 
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080, debug=True)
